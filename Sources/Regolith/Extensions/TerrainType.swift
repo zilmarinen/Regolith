cf03cc6d8744584f0110763c8d358b2f0b3f76a7
Zack Brown <zack@zackbrown.co.uk>
2023-09-30 14:36:56 +0100

    Add TerrainType extension for color palette

---
+++ Sources/Regolith/Extensions/TerrainType.swift
@@ -0,0 +1,25 @@
+//
+//  TerrainType.swift
+//
+//  Created by Zack Brown on 30/09/2023.
+//
+
+import Bivouac
+import Foundation
+
+extension TerrainType {
+    
+    public var colorPalette: ColorPalette {
+        
+        switch self {
+            
+        case .boreal: return .init(primary: .init(.systemGreen), secondary: .init(.systemBrown))
+        case .chaparral: return .init(primary: .init(.systemTeal), secondary: .init(.systemGray))
+        case .deciduous: return .init(primary: .init(.systemGreen), secondary: .init(.systemBrown))
+        case .prairie: return .init(primary: .init(.systemGreen), secondary: .init(.systemBrown))
+        case .rainforest: return .init(primary: .init(.systemGreen), secondary: .init(.systemBrown))
+        case .scrubland: return .init(primary: .init(.systemGreen), secondary: .init(.systemBrown))
+        case .tundra: return .init(primary: .init(.systemGreen), secondary: .init(.systemBrown))
+        }
+    }
+}
