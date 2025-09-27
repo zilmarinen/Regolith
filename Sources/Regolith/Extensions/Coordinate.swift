0aa1669788fded5cf2f4157728edb2213eb36622
Zack Brown <zack@zackbrown.co.uk>
2025-09-27 11:23:40 +0100

    Add coordinate unique identifier

---
+++ Sources/Regolith/Extensions/Coordinate.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinate.swift
+//  Regolith
+//
+//  Created by Zack Brown on 26/09/2025.
+//
+
+import Deltille
+
+extension Coordinate {
+    
+    public var identifier: Int { x &* .bell ^ y &* .delicate ^ z &* .mersenne }
+}
---
+++ Sources/Regolith/Extensions/Int.swift
@@ -0,0 +1,12 @@
+//
+//  Int.swift
+//
+//  Created by Zack Brown on 21/09/2025.
+//
+
+internal extension Int {
+                      
+    static let bell = 27644437
+    static let delicate = 1062599
+    static let mersenne = 57885161
+}
